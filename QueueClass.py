4b5f4a0f79a166d0acb0ca5afedf19ecec24d307
tochiepepito <pepito.tochie.ian@gmail.com>
2024-11-24 22:05:49 +0800

    Added Method (e.g addTrack, add playList and duration)

--- QueueClass.py
+++ QueueClass.py
@@ -1,6 +1,7 @@
 import json
 from TrackClass import Track
 from PlaylistClass import Playlist
+import AVLTree
 
 class Node:
     def __init__(self,track = None):
@@ -17,6 +18,7 @@ class MusicQueue:
         self.__repeat = True
         self.__playing = False  # Whether the track is currently playing
         self.__totalDuration = 0  # Total duration of all tracks in the queue
+        self.avlTree = AVLTree.AVLTree()
 
     # Accessor methods
     def getQueue(self):
@@ -54,10 +56,67 @@ class MusicQueue:
         with open("playlist.json", "r") as file:
             return json.load(file)
 
-    
+    def convertToSeconds(self, timeStr):
+        """Convert a time string (MM:SS) to total seconds without any built-in functions."""
+        colon_index = 0
+        for i in range(len(timeStr)):
+            if timeStr[i] == ":":
+                colon_index = i
+                break
+
+        minutes_str = timeStr[:colon_index]
+        seconds_str = timeStr[colon_index + 1:]
+
+        def toInt(num_str):
+            digits = "0123456789"
+            value = 0
+            for char in num_str:
+                for i in range(len(digits)):
+                    if char == digits[i]:
+                        value = value * 10 + i
+                        break
+            return value
+
+        minutes = toInt(minutes_str)
+        seconds = toInt(seconds_str)
+
+        return minutes * 60 + seconds
+
+    def formatDuration(self, total_seconds):
+        """Format duration from total seconds to 'X hr Y min Z sec' without divmod or ord."""
+        minutes = 0
+        while total_seconds >= 60:
+            total_seconds -= 60
+            minutes += 1
+
+        seconds = total_seconds
+        hours = 0
+        while minutes >= 60:
+            minutes -= 60
+            hours += 1
+
+        if hours > 0:
+            return f"{hours} hr {minutes} min {seconds} sec"
+        else:
+            return f"{minutes} min {seconds} sec"
+        
     # Add a track to the queue
-    def addTrack(self):
-        pass
+    def addTrack(self, track):
+        """Add a track to the queue."""
+        new_node = Node(track)
+        if self.__head is None:
+            self.__head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+        self.__totalDuration += self.convertToSeconds(track.getDuration())
+
+        
+    def addPlaylist(self, playlist):
+        for track in playlist:
+            self.addTrack(track)
 
     # Remove a track from the queue
     def removeTrack(self):
