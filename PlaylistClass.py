374814500f046074f6a8ce27e2e5c14d0120dd3c
crn-mese <s.berse.christinemae@cmu.edu.ph>
2024-11-25 05:22:55 +0800

    Updated playlist class

--- PlaylistClass.py
+++ PlaylistClass.py
@@ -1,6 +1,19 @@
+import json
+from TrackClass import Track
+
 class Playlist:
-    def __init__(self):
-        pass
+    def __init__(self, playlist_name: str):
+        self.__playlist_name = playlist_name
+        self.__tracks = []
+        self.__total_duration = 0
+    
+    def getPlaylistName(self):
+        return self.__playlist_name
+    
+    def getTotalDuration(self):
+        minutes = self.__total_duration // 60
+        seconds = self.__total_duration % 60
+        return f"{minutes} min {seconds} sec"
     
     def addTrack(self):
         pass
