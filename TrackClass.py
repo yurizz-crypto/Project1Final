84afd3b3bca1875b6345189b89ad5aba931b01a2
yurizz-crypto <pacaljoenel08@gmail.com>
2024-11-24 22:14:32 +0800

    TrackClass getters and input Validator Method

---
+++ TrackClass.py
@@ -0,0 +1,18 @@
+class Track:
+    def __init__(self, title: str, main_artist: str, album: str, duration: str, additional_artists: list | None = None):
+        self.__title = self.validateInput(title, "Title")
+        self.__artist = self.validateInput(main_artist, "Artist")
+        self.__album = self.validateInput(album, "Album")
+        self.__duration = duration
+        self.__additional_artists = additional_artists or []
+
+    def getTitle(self): return self.__title
+    def getArtist(self): return self.__artist
+    def getAlbum(self): return self.__album
+    def getDuration(self): return self.__duration
+    def getAdditionalArtists(self): return self.__additional_artists
+
+    def validateInput(self, value: str, field_name: str) -> str:
+        if not value:
+            raise ValueError(f"{field_name} cannot be empty.")
+        return value
