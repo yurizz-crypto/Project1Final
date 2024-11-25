744af97c171170a22d4f8d1fece652ff72382cf0
JethyHowell <s.sumalhay.jethelyn@cmu.edu.ph>
2024-11-26 00:18:20 +0800

    Implement AVL Tree rotation methods for balancing

--- AVLTree.py
+++ AVLTree.py
@@ -42,19 +42,57 @@ class AVLTree:
     def updateHeight(self, node: AVLNode):
         leftHeight = self.getHeight(node.getLeft())
         rightHeight = self.getHeight(node.getRight())
-               
+        
+        if leftHeight > rightHeight:
+            node.setHeight(leftHeight + 1)
+        else:
+            node.setHeight(rightHeight + 1)
+                   
+    def getBalanceFactor(self, node: AVLNode):
+        if node:
+            return self.get_height(node.getLeft()) - self.get_height(node.getRight())
+        return 0
 
-    def getBalanceFactor(self):
-        pass
+    def rotateRight(self, root: AVLNode):
+        rootLeft = root.getLeft()
+        rootLeftRight = rootLeft.getRight()
+        
+        rootLeft.setRight(rootLeft)
+        root.setLeft(rootLeftRight)
+        
+        self.updateHeight(root)
+        self.updateHeight(rootLeft)
+        
+        return rootLeft
 
-    def rotateRight(self):
-        pass
-
-    def rotateLeft(self):
-        pass
+    def rotateLeft(self, root: AVLNode):
+        rootRight = root.getRight()
+        rootRightLeft = rootRight.getLeft()
+        
+        rootRight.setLeft(root)
+        root.setRight(rootRightLeft)
+        
+        self.updateHeight(root)
+        self.updateHeight(rootRight)
     
-    def autoRotate(self):
-        pass
+    def autoRotate(self, node: AVLNode):
+        balance = self.get_balance_factor(node)
+        
+        if balance > 1: 
+            if self.getBalanceFactor(node.getLeft()) >= 0:
+                return self.rotateRight(node)
+        
+            else:
+                node.setLeft(self.rotateLeft(node.getLeft()))
+                return self.rotateRight(node)
+        
+        if balance < -1:
+            if self.get_balance_factor(node.getRight()) <= 0:
+                return self.rotateLeft(node)
+            
+            else:
+                node.setRight(self.rotateRight(node.getRight()))
+                return self.rotateLeft(node)
 
     def compareTracks(self):
         pass
