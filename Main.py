6abe18bec8874db29ec0a4ff5745515377891825
yurizz-crypto <pacaljoenel08@gmail.com>
2024-11-25 08:07:28 +0800

    Added the Menus dictionary and a show menu method

--- Main.py
+++ Main.py
@@ -3,4 +3,44 @@ from AVLTree import AVLTree
 from PlaylistClass import Playlist
 from QueueClass import MusicQueue
 
-menus = {}
+MENUS = {
+    "main": {
+        1 : "Library",
+        2 : "Playlists",
+        3 : "Queue",
+        4 : "Now Playing",
+        5 : "Exit"
+    },
+    "musicLibrary": {
+        1 : "Add a New Track",
+        2 : "View All Tracks",
+        3 : "Search for a Track",
+        4 : "Delete a Track",
+        5 : "Return to Main Menu",
+    },
+    "playlists": {
+        1 : "Create a New Playlist",
+        2 : "View All Playlist",
+        3 : "Add Tracks to a Playlist",
+        4 : "Delete a Playlist",
+        5 : "Display a Playlist",
+        6 : "Return to Main Menu"
+    },
+    "queue" : {
+        1 : "Play",
+        2 : "Next",
+        3 : "Previous",
+        4 : "Shuffle",
+        5 : "Repeat",
+        6 : "Clear Queue",
+        7 : "Exit"
+    }   
+}
+
+def showMenu(menu: str) -> str:
+    if menu in MENUS:
+        keys = list(MENUS[menu])
+        for i in range(len(keys)):
+            print("[{}]".format(str(keys[i])) + " " + MENUS[menu][keys[i]])
+    else:
+        print("Menu not found.")
